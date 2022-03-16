6acbe25ded1300ef905ceb59a6382debd7c7f1be
fabercheng <59500128+fabercheng@users.noreply.github.com>
2022-03-16 20:06:55 +0800

    Update word-tokenize.py

--- TM/Data_cl/word-tokenize.py
+++ TM/Data_cl/word-tokenize.py
@@ -1,7 +1,7 @@
 import nltk
 from nltk.tag import pos_tag
 from nltk.tokenize import word_tokenize
-f = open('/Users/devonne/Documents/JNU/NLPmodel/BiLSTM+CRF/v2.0/output.txt')
+f = open('~/output.txt')
 cb = f.read()
 text = word_tokenize(cb)
 
